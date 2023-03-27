aca1c3da1dc86a6d5eeac2ee83f64dc1930326bc
Adrien Bérard <adri.brrd@outlook.fr>
2023-03-27 11:02:41 +0200

    bug resolu

--- pwa/docpatient/vite.config.js
+++ pwa/docpatient/vite.config.js
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [
     react(),
