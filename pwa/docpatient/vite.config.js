ed2156b019bde124df1954c4b0f54ddca1ea0ee7
Adrien Bérard <adri.brrd@outlook.fr>
2023-03-27 10:54:54 +0200

    test

--- pwa/docpatient/vite.config.js
+++ pwa/docpatient/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "DocPatient",
+        short_name: 'DP',
+        description: 'L\'application pour les docteurs et les patients !',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
